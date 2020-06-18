12236e12fc1524f490c9c68881c094d12829bcdc
gamagrafia <flavio.targaryen@gmail.com>
2020-06-17 21:45:18 -0300

    primeiro commit

---
+++ exemplo.js
@@ -0,0 +1,9 @@
+let var1= window.prompt("Qual o primeiro número?")
+
+let var2= window.prompt("Qual o segundo número?")
+
+if (var1 > var2) { alert( 'O maior número é ' + var1 )}
+else alert ('O Maior número é' + var2 )
+
+
+
